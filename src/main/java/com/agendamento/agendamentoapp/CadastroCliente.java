6e72ae0d5d0a1819f1b1f0929036de94391ce790
Leandro_soares <119981487+Sato-Reandoro@users.noreply.github.com>
2023-10-16 10:56:37 -0300

    otimização das Classes "Clientes" e "CadastroClientes"

--- src/main/java/com/agendamento/agendamentoapp/CadastroCliente.java
+++ src/main/java/com/agendamento/agendamentoapp/CadastroCliente.java
@@ -1,42 +1,42 @@
 package com.agendamento.agendamentoapp;
+
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+
 
 public class CadastroCliente {
     public static void main(String[] args) {
-        ArrayList<Clientes> listaCliente = new ArrayList<>();
+        ArrayList<Clientes> listaClientes = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("Cadastro de Cliente");
-            System.out.println("1. Cadastrar novo Cliente");
-            System.out.println("2. Listar Cliente");
+            System.out.println("Cadastro de Clientes");
+            System.out.println("1. Cadastrar novo cliente");
+            System.out.println("2. Listar clientes");
             System.out.println("3. Sair");
             System.out.print("Escolha uma opção: ");
+
             int opcao = scanner.nextInt();
             scanner.nextLine(); // Limpa o buffer do scanner
 
             switch (opcao) {
                 case 1:
                     System.out.println("Cadastro de novo cliente");
-                    System.out.print("numProntuario");
-                    String numProntuario = scanner.nextLine();
-
-                    
-                    System.out.print("nome");
+                    System.out.print("Nome: ");
                     String nome = scanner.nextLine();
 
-                     System.out.print(" tipoPessoa");
-                    String tipoPessoa = scanner.nextLine();
+                    System.out.print("Naturalidade: ");
+                    String naturalidade = scanner.nextLine();
 
                     System.out.print("CPF: ");
                     String cpf = scanner.nextLine();
                     if (!validarCPF(cpf)) {
                         System.out.println("CPF inválido. Por favor, insira um CPF válido.");
-                        continue; // Volte ao início do loop para que o usuário insira novamente.
+                        continue;
                     }
 
-                   System.out.print("dataNascimento ");
+                    System.out.print("Data de Nascimento: ");
                     String dataNascimento = scanner.nextLine();
 
                     System.out.print("Endereço: ");
@@ -48,30 +48,33 @@ public class CadastroCliente {
                     System.out.print("Cidade: ");
                     String cidade = scanner.nextLine();
 
-                    System.out.print(" Estado");
+                    System.out.print("Estado: ");
                     String estado = scanner.nextLine();
 
-                    System.out.print("sexo");
-                    String sexo= scanner.nextLine();
+                    System.out.print("Sexo: ");
+                    String sexo = scanner.nextLine();
 
-                    System.out.print("Tipo Sanguineo");
+                    System.out.print("Tipo Sanguíneo: ");
                     String tipoSanguineo = scanner.nextLine();
 
+                    System.out.print("Senha: ");
+                    String senha = scanner.nextLine();
+                    if (!validarSenha(senha)) {
+                        System.out.println("Senha inválida. A senha deve conter uma letra maiúscula, uma letra minúscula e um caractere especial.");
+                        continue;
+                    }
 
+                    int novoId = listaClientes.size() + 1;
+                    Clientes novoCliente = new Clientes(novoId, nome, naturalidade, cpf, dataNascimento, endereco, bairro, cidade, estado, sexo, tipoSanguineo, senha);
+                    listaClientes.add(novoCliente);
 
-
-                    
-
-                    Clientes novoCliente = new Cliente(numProntuario, nome, tipoPessoa, cpf, dataNascimento, bairro, cidade, estado, sexo, tipoSanguineo);
-                    listaCliente.add(novoCliente);
-
-                    System.out.println(" Cliente cadastrado com sucesso!");
+                    System.out.println("Cliente cadastrado com sucesso!");
                     break;
 
                 case 2:
-                    System.out.println("Lista de Cliente:");
-                    for (Clientes cliente : listaCliente) {
-                        System.out.println(Cliente);
+                    System.out.println("Lista de Clientes:");
+                    for (Clientes cliente : listaClientes) {
+                        System.out.println(cliente);
                     }
                     break;
 
@@ -85,38 +88,18 @@ public class CadastroCliente {
             }
         }
     }
-// Função para validar CPF
-public static boolean validarCPF(String cpf) {
-    // Remove qualquer caractere não numérico do CPF
-    cpf = cpf.replaceAll("[^0-9]", "");
 
-    // Verifica se o CPF tem 11 dígitos
-    if (cpf.length() != 11) {
-        return false;
+    // Método de validação de CPF
+    public static boolean validarCPF(String cpf) {
+        String regex = "^[0-9]{11}$";
+        return Pattern.matches(regex, cpf);
     }
 
-    // Calcula os dígitos verificadores
-    int soma = 0;
-    for (int i = 0; i < 9; i++) {
-        soma += Character.getNumericValue(cpf.charAt(i)) * (10 - i);
+    // Método de validação de senha
+    public static boolean validarSenha(String senha) {
+        // A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula e um caractere especial
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+        return Pattern.matches(regex, senha);
     }
-    int primeiroDigito = 11 - (soma % 11);
-    if (primeiroDigito >= 10) {
-        primeiroDigito = 0;
-    }
-
-    soma = 0;
-    for (int i = 0; i < 10; i++) {
-        soma += Character.getNumericValue(cpf.charAt(i)) * (11 - i);
-    }
-    int segundoDigito = 11 - (soma % 11);
-    if (segundoDigito >= 10) {
-        segundoDigito = 0;
-    }
-
-    // Verifica se os dígitos verificadores são iguais aos dígitos do CPF
-    return (Character.getNumericValue(cpf.charAt(9)) == primeiroDigito &&
-            Character.getNumericValue(cpf.charAt(10)) == segundoDigito);
-}
-
+    
 }
--- src/main/java/com/agendamento/agendamentoapp/Clientes.java
+++ src/main/java/com/agendamento/agendamentoapp/Clientes.java
@@ -1,83 +1,146 @@
 package com.agendamento.agendamentoapp;
 
-public class Clientes{
-    
-    private String numProntuario;
+public class Clientes {
+    private int id;
     private String nome;
-    private String tipoPessoa;
+    private String naturalidade;
     private String cpf;
     private String dataNascimento;
-    private String endereço;
+    private String endereco;
     private String bairro;
     private String cidade;
-    private String estado; 
+    private String estado;
     private String sexo;
     private String tipoSanguineo;
-    
-    public String getNumProntuario() {
-        return numProntuario;
+    private String senha;
+
+   
+
+    // Construtor
+    public Clientes(int id, String nome, String naturalidade, String cpf, String dataNascimento, String endereco, String bairro, String cidade, String estado, String sexo, String tipoSanguineo, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.naturalidade = naturalidade;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.endereco = endereco;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.sexo = sexo;
+        this.tipoSanguineo = tipoSanguineo;
+        this.senha = senha;
     }
-    public void setNumProntuario(String numProntuario) {
-        this.numProntuario = numProntuario;
+
+    // Getters e Setters para todos os campos
+ public int getId() {
+        return id;
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
-    public String getTipoPessoa() {
-        return tipoPessoa;
+
+    public String getNaturalidade() {
+        return naturalidade;
     }
-    public void setTipoPessoa(String tipoPessoa) {
-        this.tipoPessoa = tipoPessoa;
+
+    public void setNaturalidade(String naturalidade) {
+        this.naturalidade = naturalidade;
     }
+
     public String getCpf() {
         return cpf;
     }
+
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
-        public String getDataNascimento() {
+
+    public String getDataNascimento() {
         return dataNascimento;
     }
+
     public void setDataNascimento(String dataNascimento) {
         this.dataNascimento = dataNascimento;
     }
-    public String getEndereço() {
-        return endereço;
+
+    public String getEndereco() {
+        return endereco;
     }
-    public void setEndereço(String endereço) {
-        this.endereço = endereço;
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
     }
+
     public String getBairro() {
         return bairro;
     }
+
     public void setBairro(String bairro) {
         this.bairro = bairro;
     }
+
     public String getCidade() {
         return cidade;
     }
+
     public void setCidade(String cidade) {
         this.cidade = cidade;
     }
+
     public String getEstado() {
         return estado;
     }
+
     public void setEstado(String estado) {
         this.estado = estado;
     }
+
     public String getSexo() {
         return sexo;
     }
+
     public void setSexo(String sexo) {
         this.sexo = sexo;
     }
+
     public String getTipoSanguineo() {
         return tipoSanguineo;
     }
+
     public void setTipoSanguineo(String tipoSanguineo) {
         this.tipoSanguineo = tipoSanguineo;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    // Sobrescrevendo o método toString para exibir informações do cliente
+    @Override
+    public String toString() {
+        return "Cliente: " + nome +
+               "\nID: " + id +
+               "\nNaturalidade: " + naturalidade +
+               "\nCPF: " + cpf +
+               "\nData de Nascimento: " + dataNascimento +
+               "\nEndereço: " + endereco +
+               "\nBairro: " + bairro +
+               "\nCidade: " + cidade +
+               "\nEstado: " + estado +
+               "\nSexo: " + sexo +
+               "\nTipo Sanguíneo: " + tipoSanguineo;
     }
 }
